c4137e94c4ef8eaf8e60e24218ebec7c54133cc5
samujjwal-patra <samujjwalpatra@gmail.com>
2025-09-05 14:01:27 +0530

    Complete Inventory.js

---
+++ fortixController/routes/inventoryRoutes.js
@@ -0,0 +1,10 @@
+// const express = require(`express`);
+// const router = express.Router();
+// const ictrl = require(`../controller/inventoryController`);
+
+// router.post("/",ictrl.createInventory);
+// router.get("/",ictrl.getInventory);
+// router.put("/:id",ictrl.updateInventory);
+// router.delete("/:id",ictrl.deleteInventory);
+
+// module.exports = router;
