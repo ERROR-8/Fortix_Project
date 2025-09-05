7874ea2989c05f1e633a90ef4aee259ff6a18978
samujjwal-patra <samujjwalpatra@gmail.com>
2025-09-05 14:24:39 +0530

    Add somechanges in inventory.js

--- fortixController/modules/inventory.js
+++ fortixController/modules/inventory.js
@@ -1,11 +1,11 @@
-// const mongoose = require(`mongoose`);
+const mongoose = require(`mongoose`);
 
-// const inventorySchema = new mongoose.Schema({
-//     productName: { type: String, required: true},
-//     category: { type: String, required: true},
-//     price: { type: Number, required: true},
-//     expDate: { type: String, required: true},
-//     quantity: { type: Number, required: true},
-// })
+const inventorySchema = new mongoose.Schema({
+    productName: { type: String, required: true},
+    category: { type: String, required: true},
+    price: { type: Number, required: true},
+    expDate: { type: String, required: true},
+    quantity: { type: Number, required: true},
+})
 
-// module.exports = mongoose.model("Inventory",inventorySchema);
+module.exports = mongoose.model("Inventory",inventorySchema);
