db9d39b27fdd33c613b714b6ab9312990f2e1264
samujjwal-patra <samujjwalpatra@gmail.com>
2025-09-05 14:20:49 +0530

    Test Api

--- fortixController/server.js
+++ fortixController/server.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const cors = require(`cors`);
 const dotenv = require(`dotenv`);
 const connectdb = require(`./config/db`);
+const inventoryRoutes = require(`./routes/inventoryRoutes`);
 
 dotenv.config();
 const app = express();
@@ -13,8 +14,9 @@ app.get("/",(req,res) =>{
     res.send("Api is Working");
 });
 
-const port = process.env.PORT || 4444;
+app.use("/api/inventory",inventoryRoutes);
 
+const port = process.env.PORT || 4444;
 app.listen(port, () =>{
     console.log("Server is Running at Port 4444");
 });
