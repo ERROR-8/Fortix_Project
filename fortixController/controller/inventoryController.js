1dbe56bf4f8615afc7ed9ff771a80b30fb354e27
samujjwal-patra <samujjwalpatra@gmail.com>
2025-09-05 14:01:51 +0530

    Complete InventoryController.js

--- fortixController/controller/inventoryController.js
+++ fortixController/controller/inventoryController.js
@@ -1,37 +1,37 @@
-// const Inventory = require(`../modules/inventory`);
+const Inventory = require(`../modules/inventory`);
 
-// exports.createInventory = async(req,res) => {
-//     try {
-//         const inventory = await Inventory.create(req.body);
-//         res.json(inventory);
-//     } catch(err) {
-//         res.json(err);
-//     }
-// };
+exports.createInventory = async(req,res) => {
+    try {
+        const inventory = await Inventory.create(req.body);
+        res.json(inventory);
+    } catch(err) {
+        res.json(err);
+    }
+};
 
-// exports.getInventory = async(req,res) => {
-//     try {
-//         const inventory = await Inventory.find();
-//         res.json(inventory);
-//     } catch(err) {
-//         res.json(err);
-//     }
-// };
+exports.getInventory = async(req,res) => {
+    try {
+        const inventory = await Inventory.find();
+        res.json(inventory);
+    } catch(err) {
+        res.json(err);
+    }
+};
 
-// exports.updateInventory = async(req,res) => {
-//     try {
-//         const inventory = await Inventory.findByIdAndUpdate(req.params.id,req.body,{new:true});
-//         res.json(inventory);
-//     } catch(err) {
-//         res.json(err);
-//     }
-// };
+exports.updateInventory = async(req,res) => {
+    try {
+        const inventory = await Inventory.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.json(inventory);
+    } catch(err) {
+        res.json(err);
+    }
+};
 
-// exports.deleteInventory = async(req,res) => {
-//     try {
-//         await Inventory.findByIdAndDelete(req.params.id);
-//         res.json({message: "Inventory Deleted Succesfully"});
-//     } catch(err) {
-//         res.json(err);
-//     }
-// };
+exports.deleteInventory = async(req,res) => {
+    try {
+        await Inventory.findByIdAndDelete(req.params.id);
+        res.json({message: "Inventory Deleted Succesfully"});
+    } catch(err) {
+        res.json(err);
+    }
+};
